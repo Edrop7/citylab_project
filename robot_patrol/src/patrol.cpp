82d65dd52ca0a195f7f90527e8e8747fc5ebb335
Edrop7 <samovalle878@gmail.com>
2025-04-07 18:17:43 +0000

    separated publisher to its own callback using a timer, detected bug when trying to complete counterclockwise lap (robot crashes when approaching wall), need to fix

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -14,21 +14,27 @@ public:
         sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>
             (
             "/scan", 
-            10, // HZ
+            10,
             std::bind(&Patrol::callback_laser, this, std::placeholders::_1)
             );
 
-        // 6. CREATE A CONTROL LOOP OF 10 HZ
         pub_ = this->create_publisher<geometry_msgs::msg::Twist>
             (
             "/cmd_vel", 
-            10 // HZ
+            10
             );
+
+        // 6. CREATE A CONTROL LOOP OF 10 HZ
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100),  // 10 Hz -> run every 100 ms (10 times a second)
+            std::bind(&Patrol::callback_velocity, this) // callback for publisher separated
+        );
     }
 
 private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
     std::vector<float> front_laser_scan;
     std::map<std::string, float> laser_reading_map;
@@ -37,6 +43,7 @@ private:
     float angular_z_vel_ = 0.0;
 
     float direction_ = 0.0;
+    float min_distance_allowed_ = 0.35; //in m
 
     void callback_laser(const sensor_msgs::msg::LaserScan::SharedPtr msg)
     {
@@ -46,7 +53,6 @@ private:
         this->laser_reading_map["front"] = msg->ranges[359];
 
         stateflow_navigator(this->laser_reading_map, this->front_laser_scan);
-        publish_velocities();
     }
 
     // 5. ALGORITHM TO IMPLEMENT
@@ -57,17 +63,14 @@ private:
         int max_index = 0;
 
         // 5. MOVE THE ROBOT FORWARD UNTIL AN OBSTACLE IN FRONT OF ROBOT IS CLOSER THAN 35 CM
-        if (laser_reading_map.at("front") > 0.35)
+        if (laser_reading_map.at("front") > this->min_distance_allowed_)
         {
             this->angular_z_vel_ = 0.0;
             RCLCPP_INFO(this->get_logger(), "No frontal obstacle, cm to next obstacle: %f",
                                             laser_reading_map.at("front"));
         }
-        else if (laser_reading_map.at("front") < 0.35)
+        else if (laser_reading_map.at("front") < this->min_distance_allowed_)
         {
-            RCLCPP_INFO(this->get_logger(), "OBSTACLE DETECTED WITHIN: %f",
-                                            laser_reading_map.at("front"));
-
             // 5. DETERMINE WHICH RAY IN FRONTAL 180 IS THE LARGEST BUT NOT INFINITE
             for (int index = 0; index < 360; index++) {
                 distance_of_index = front_laser_scan[index];
@@ -80,14 +83,19 @@ private:
             this->laser_reading_map["max"] = max_distance; // in case I want to log it
 
             // 4. & 5. DETERMINE WHAT IS THE SAFEST DIRECTION GIVEN THE MAX INDEX (radians)
-            this->direction_ = -(3.141592/2) + (max_index * (3.141592/180));
+            this->direction_ = (-3.141592/2) + (max_index * (3.141592/180));
 
             // 5. ANGULAR VELOCITY = DIRECTION (radians) / 2 (rad/s)
             this->angular_z_vel_ = this->direction_ / 2;
+
+            RCLCPP_INFO(this->get_logger(), "OBSTACLE DETECTED WITHIN: %f cm;   VELOCITY SENT: %f rad/s",
+                                            laser_reading_map.at("front"),
+                                            this->angular_z_vel_);
         }
     }
-    // 6. CREATE A CONTROL LOOP OF 10 HZ (SEE CONSTRUCTOR)
-    void publish_velocities()
+
+    // 6. CREATE A CONTROL LOOP OF 10 HZ
+    void callback_velocity()
     {
         auto msg = geometry_msgs::msg::Twist();
         msg.linear.x = this->linear_x_vel_;

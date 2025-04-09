2bc08c2cec6c31a58a1bad02360d15e51bf8950a
Edrop7 <samovalle878@gmail.com>
2025-04-09 16:08:25 +0000

    CHECKPOINT COMPLETE IN SIM: rviz config file created in new rviz dir within pkg, and referenced to launch within launch file. Tested patrol and rviz visualization in both directions (CW, CCW) in simulation successfully

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,10 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'rviz',
+        'config.rviz'
+    )
+
     return LaunchDescription([
+        # Patrol
         Node(
             package='robot_patrol',
             executable='patrol_node',
             output='screen'),
+
+        # RVIZ
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path]
+        )
     ])
